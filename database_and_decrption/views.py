f6e6a3e3387b5bca07c1eaefd96bde071f7614df
Hamza Bilal <hamzaabialal@gmail.com>
2024-09-01 14:50:03 +0500

    Added 2nd

--- database_and_decrption/views.py
+++ database_and_decrption/views.py
@@ -42,14 +42,12 @@ def decrypt_image(request):
             return JsonResponse({'error': 'Encrypted image not found.'})
 
         s_box = encrypted_image_record.s_box
-        # Ensure s_box is a list, not a JSON string
         if isinstance(s_box, str):
             s_box = json.loads(s_box)
 
         json_data = json.loads(encrypted_image_record.json_data)
         encrypted_image_path = encrypted_image_record.encrypted_image_path
 
-        # Generate the inverse S-box
         inverse_s_box = [0] * 256
         for i in range(256):
             inverse_s_box[s_box[i]] = i
@@ -64,9 +62,7 @@ def decrypt_image(request):
         custom_aes = CustomAES(aes_key)
         block_size = 16
 
-
         try:
-            # Paste decrypted blocks onto the existing encrypted image
             for block_info in json_data["blocks"]:
                 x, y = block_info["x"], block_info["y"]
                 encrypted_block = bytes.fromhex(block_info["encrypted_block"])
@@ -80,16 +76,15 @@ def decrypt_image(request):
                     print(f"Image error: {e}")
 
             # Save the decrypted image
-            decrypted_folder = os.path.join(settings.BASE_DIR, 'media')
+            decrypted_folder = os.path.join(settings.MEDIA_ROOT, 'decrypted')
             os.makedirs(decrypted_folder, exist_ok=True)
             decrypted_image_path = os.path.join(decrypted_folder, os.path.basename(encrypted_image_path))
-            decrypted_image_path = decrypted_image_path.split('/media')[1]
-            decrypted_image_path = 'media' + decrypted_image_path
 
             encrypted_image.save(decrypted_image_path)
 
-            print("decrypted_image_path", decrypted_image_path)
+            # Construct the media-relative path to return in the response
+            decrypted_image_url = os.path.join(settings.MEDIA_URL, 'decrypted', os.path.basename(encrypted_image_path))
 
-            return JsonResponse({'success': True, 'decrypted_image_path': decrypted_image_path})
+            return JsonResponse({'success': True, 'decrypted_image_path': decrypted_image_url})
         except Exception as e:
             return JsonResponse({'error': f"An error occurred during decryption: {e}"})
