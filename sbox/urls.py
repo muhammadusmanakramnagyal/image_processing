f44ec0ce1b299e9ca9eef9dc42f12b0f5a60c36e
Hamza Bilal <hamzaabialal@gmail.com>
2024-09-12 02:28:27 +0500

    Added

--- sbox/urls.py
+++ sbox/urls.py
@@ -4,5 +4,5 @@ from .views import generate_quantum_sbox,show_sbox_page, IndexPageView
 urlpatterns = [
     path('sbox/', show_sbox_page, name='show_sbox_page'),
     path('generate_quantum_sbox/', generate_quantum_sbox, name='generate_quantum_sbox'),
-    path('', IndexPageView.as_view(), name='index_page'),
+    path('index_page/', IndexPageView.as_view(), name='index_page'),
 ]
